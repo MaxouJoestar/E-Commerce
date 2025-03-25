dedcfcaa1066c1d95500afce51eccd0c6de7a1a4
MaxouJoestar <maxime.bro@ynov.com>
2025-03-25 15:34:21 +0100

    maven

---
+++ poogroupe/demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Bienvenue sur notre site e-commerce !");
+        return "index"; // Correspond au fichier templates/index.html
+    }
+}
